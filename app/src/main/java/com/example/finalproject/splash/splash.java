f2c2631bba9c9391531d742d5161f6cb4407f4bd
hp <ia.amassi@alaqsa.edu.ps>
2022-05-08 21:20:07 +0200

    In this project, I added Splash with the addition of a library to run Splash

---
+++ app/src/main/java/com/example/finalproject/splash/splash.java
@@ -0,0 +1,29 @@
+package com.example.finalproject.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.example.finalproject.MainActivity;
+import com.example.finalproject.R;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
