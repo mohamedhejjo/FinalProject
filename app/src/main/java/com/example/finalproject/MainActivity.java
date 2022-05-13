c6a858afc5764dfed798b372e7d83f46ea5d8bc0
hp <ia.amassi@alaqsa.edu.ps>
2022-05-13 22:57:38 +0200

    قمت باضافة قائمة لتغيير لغة التطبيق

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.finalproject.admin.Clothesa.addcolthes;
+import com.example.finalproject.admin.Clothesa.adddevices;
 import com.example.finalproject.admin.admin;
+import com.example.finalproject.admin.adminCategories;
 import com.example.finalproject.user.user;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,5 +42,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.lang,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.Arabic){
+         setLocale("ar");
+
+        }else if(id==R.id.Einglish){
+           setLocale("en");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @SuppressWarnings("deprecation")
+    public  void setLocale(String lang){
+        Locale mylocale=new Locale(lang);
+        DisplayMetrics dm=getResources().getDisplayMetrics();
+        Configuration conf=getResources().getConfiguration();
+        conf.locale=mylocale;
+        getResources().updateConfiguration(conf,dm);
+        Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
