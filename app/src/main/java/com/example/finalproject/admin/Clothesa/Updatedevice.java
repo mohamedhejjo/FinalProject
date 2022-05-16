2f0d124b593a1b5e89732277c4fce7adb04ffcb8
hp <ia.amassi@alaqsa.edu.ps>
2022-05-16 23:24:35 +0200

    تعديل بسيط

--- app/src/main/java/com/example/finalproject/admin/Clothesa/Updatedevice.java
+++ app/src/main/java/com/example/finalproject/admin/Clothesa/Updatedevice.java
@@ -19,10 +19,10 @@ public class Updatedevice extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.updatedevice);
         ArrayList<ClassShowPrice> data=new ArrayList<>();
-        data.add(new ClassShowPrice("male",R.drawable.male,"100","ee"));
-        data.add(new ClassShowPrice("female",R.drawable.female,"200","ee"));
-        data.add(new ClassShowPrice("male",R.drawable.male,"500","ee"));
-        data.add(new ClassShowPrice("female",R.drawable.female,"800","ee"));
+        data.add(new ClassShowPrice("male",R.drawable.male,"100","laptop"));
+        data.add(new ClassShowPrice("female",R.drawable.female,"200","mouse"));
+        data.add(new ClassShowPrice("male",R.drawable.male,"500","keyboard"));
+        data.add(new ClassShowPrice("female",R.drawable.female,"800","playstation"));
         RecyclerView rv=findViewById(R.id.rec6);
         AdapterShowPrice ad=new AdapterShowPrice(data);
         rv.setAdapter(ad);
